0570a5341ecacf61ff8fa4be0c46b49eed676093
Mark Cornick <mark@markcornick.com>
2022-12-02 21:05:51 -0500

    Use argparse

--- genpw.py
+++ genpw.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import string
 import sys
 
@@ -8,18 +9,21 @@ except ModuleNotFoundError:
     sys.stderr.write("This script requires Python 3.6 or newer\n")
     sys.exit(1)
 
-try:
-    length = int(sys.argv[1])
-except (IndexError, ValueError):
-    length = 16
+parser = argparse.ArgumentParser(
+    prog="genpw.py", description="a random password generator"
+)
+parser.add_argument(
+    "-l", "--length", default=16, type=int, help="length to generate (default: 16)"
+)
+args = parser.parse_args()
 
-if length < 3:
-    sys.stderr.write("Minimum length is 3\n")
+if args.length < 3:
+    sys.stderr.write(parser.prog + ": error: minimum length is 3\n")
     sys.exit(1)
 
 alphabet = string.ascii_letters + string.digits
 while True:
-    password = "".join(secrets.choice(alphabet) for i in range(length))
+    password = "".join(secrets.choice(alphabet) for i in range(args.length))
     if (
         any(c.islower() for c in password)
         and any(c.isupper() for c in password)
