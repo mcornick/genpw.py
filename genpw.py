79e4ccf826c8c3e8e1c2475f749bfe4e7efd232f
Mark Cornick <mark@markcornick.com>
2022-12-02 20:33:54 -0500

    Include an uppercase letter, lowercase letter, and digit

--- genpw.py
+++ genpw.py
@@ -8,5 +8,12 @@ try:
 except (IndexError, ValueError):
     length = 16
 alphabet = string.ascii_letters + string.digits
-password = "".join(secrets.choice(alphabet) for i in range(length))
+while True:
+    password = "".join(secrets.choice(alphabet) for i in range(length))
+    if (
+        any(c.islower() for c in password)
+        and any(c.isupper() for c in password)
+        and any(c.isdigit() for c in password)
+    ):
+        break
 print(password)
