ccfe0e3e595559d72b89b9487043500f756f84c7
Mark Cornick <mcornick@mcornick.com>
2022-12-09 15:54:30 -0500

    Rewrite using click

--- genpw.py
+++ genpw.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Password generator using the secrets module."""
-import argparse
 import string
 import sys
+
+import click
 
 try:
     import secrets
@@ -11,65 +12,35 @@ except ModuleNotFoundError:
     sys.exit(1)
 
 
-def genpw(length, no_upper, no_lower, no_digit):
-    """Generate a password."""
+@click.command()
+@click.option("--length", default=16, help="length to generate")
+@click.option("--upper/--no-upper", default=True, help="include uppercase letters")
+@click.option("--lower/--no-lower", default=True, help="include lowercase letters")
+@click.option("--digit/--no-digit", default=True, help="include digits")
+def cli(length, upper, lower, digit):
+    """Generate a random password."""
+    if not lower and not upper and not digit:
+        raise click.ClickException("excluded all character types")
 
-    if no_lower and no_upper and no_digit:
-        return "error: excluded all character types"
-
-    minimum_length = int(not no_lower) + int(not no_upper) + int(not no_digit)
+    minimum_length = int(lower) + int(upper) + int(digit)
     if length < minimum_length:
-        return "error: length is too short"
+        raise click.ClickException("minimum length is {}".format(minimum_length))
 
     alphabet = ""
-    if not no_lower:
+    if lower:
         alphabet = alphabet + string.ascii_lowercase
-    if not no_upper:
+    if upper:
         alphabet = alphabet + string.ascii_uppercase
-    if not no_digit:
+    if digit:
         alphabet = alphabet + string.digits
 
     while True:
         password = "".join(secrets.choice(alphabet) for i in range(length))
         if (
-            (any(c.islower() for c in password) or no_lower)
-            and (any(c.isupper() for c in password) or no_upper)
-            and (any(c.isdigit() for c in password) or no_digit)
+            (any(c.islower() for c in password) or not lower)
+            and (any(c.isupper() for c in password) or not upper)
+            and (any(c.isdigit() for c in password) or not digit)
         ):
             break
-    return password
 
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog="genpw.py", description="a random password generator"
-    )
-    parser.add_argument(
-        "-l", "--length", default=16, type=int, help="length to generate (default: 16)"
-    )
-    parser.add_argument(
-        "-U",
-        "--no-upper",
-        action="store_true",
-        help="exclude uppercase letters (default: False)",
-    )
-    parser.add_argument(
-        "-u",
-        "--no-lower",
-        action="store_true",
-        help="exclude lowercase letters (default: False)",
-    )
-    parser.add_argument(
-        "-d",
-        "--no-digit",
-        action="store_true",
-        help="exclude digits (default: False)",
-    )
-    args = parser.parse_args()
-
-    password = genpw(args.length, args.no_upper, args.no_lower, args.no_digit)
-    if "error:" in password:
-        sys.stderr.write("{}: {}\n".format(parser.prog, password))
-        sys.exit(1)
-    else:
-        print(password)
+    click.echo(password)
