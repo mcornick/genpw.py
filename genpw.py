bc4cfa27357a7c050a8048eade6ec642edb238cd
Mark Cornick <mark@markcornick.com>
2022-12-11 08:50:19 -0600

    Restore separate genpw() method

--- genpw.py
+++ genpw.py
@@ -12,20 +12,8 @@ except ModuleNotFoundError:
     sys.exit(1)
 
 
-@click.command()
-@click.option("--length", default=16, show_default=True, help="length to generate")
-@click.option("--upper/--no-upper", default=True, help="include/exclude uppercase")
-@click.option("--lower/--no-lower", default=True, help="include/exclude lowercase")
-@click.option("--digit/--no-digit", default=True, help="include/exclude digits")
-def cli(length, upper, lower, digit):
-    """Generate a random password."""
-    if not lower and not upper and not digit:
-        raise click.ClickException("excluded all character types")
-
-    minimum_length = int(lower) + int(upper) + int(digit)
-    if length < minimum_length:
-        raise click.ClickException("minimum length is {}".format(minimum_length))
-
+def genpw(length=16, upper=True, lower=True, digit=True):
+    """Generate a password."""
     alphabet = ""
     if lower:
         alphabet = alphabet + string.ascii_lowercase
@@ -42,5 +30,36 @@ def cli(length, upper, lower, digit):
             and (any(c.isdigit() for c in password) or not digit)
         ):
             break
+    alphabet = ""
+    if lower:
+        alphabet = alphabet + string.ascii_lowercase
+    if upper:
+        alphabet = alphabet + string.ascii_uppercase
+    if digit:
+        alphabet = alphabet + string.digits
 
-    click.echo(password)
+    while True:
+        password = "".join(secrets.choice(alphabet) for i in range(length))
+        if (
+            (any(c.islower() for c in password) or not lower)
+            and (any(c.isupper() for c in password) or not upper)
+            and (any(c.isdigit() for c in password) or not digit)
+        ):
+            return password
+
+
+@click.command()
+@click.option("--length", default=16, show_default=True, help="length to generate")
+@click.option("--upper/--no-upper", default=True, help="include/exclude uppercase")
+@click.option("--lower/--no-lower", default=True, help="include/exclude lowercase")
+@click.option("--digit/--no-digit", default=True, help="include/exclude digits")
+def cli(length, upper, lower, digit):
+    """Generate a random password."""
+    if not lower and not upper and not digit:
+        raise click.ClickException("excluded all character types")
+
+    minimum_length = int(lower) + int(upper) + int(digit)
+    if length < minimum_length:
+        raise click.ClickException("minimum length is {}".format(minimum_length))
+
+    click.echo(genpw(length, upper, lower, digit))
